f6a197ef47e712c9872355021895a54bf73f4120
MoonStar <moonsung0331@gmail.com>
2023-06-29 08:30:29 +0900

    feat: 게시글 작성을 위한 Controller 및 Service Logic 작성

--- src/main/java/com/hackathon/going/domain/post/controller/PostController.java
+++ src/main/java/com/hackathon/going/domain/post/controller/PostController.java
@@ -2,17 +2,19 @@ package com.hackathon.going.domain.post.controller;
 
 import com.hackathon.going.domain.common.ResponseDto;
 import com.hackathon.going.domain.post.dto.PostDto;
+import com.hackathon.going.domain.post.request.PostCreateRequest;
+import com.hackathon.going.domain.post.response.PostResponse;
 import com.hackathon.going.domain.post.service.PostService;
 import com.hackathon.going.domain.travel.dto.TravelDto;
 import com.hackathon.going.domain.travel.response.HomeResponse;
+import com.hackathon.going.domain.travel.response.TravelResponse;
 import com.hackathon.going.domain.travel.service.TravelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -26,6 +28,12 @@ public class PostController {
     public ResponseDto<HomeResponse> home(Authentication authentication) {
         TravelDto myTravel = travelService.getMyTravel(authentication.getName());
         List<PostDto> posts = postService.getPosts();
-        return new ResponseDto<>(new HomeResponse(myTravel, posts));
+        return new ResponseDto<>(new HomeResponse(TravelResponse.fromDto(myTravel), posts.stream().map(PostResponse::fromDto).collect(Collectors.toList())));
+    }
+
+    @PostMapping
+    public ResponseDto<Void> createPost(Authentication authentication, @RequestBody PostCreateRequest request) {
+        postService.create(request.getTitle(), request.getContent(), request.getFiles(), authentication.getName());
+        return new ResponseDto<>();
     }
 }
