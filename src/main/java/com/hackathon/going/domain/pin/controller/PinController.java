66a41669c0bc57d2ebe9d63260e67a133400aeca
siyeonSon <siyeon0209@naver.com>
2023-06-29 06:17:44 +0900

    feat: pin 생성 API

---
+++ src/main/java/com/hackathon/going/domain/pin/controller/PinController.java
@@ -0,0 +1,28 @@
+package com.hackathon.going.domain.pin.controller;
+
+import com.hackathon.going.domain.common.ResponseDto;
+import com.hackathon.going.domain.pin.dto.request.PinRequestDto;
+import com.hackathon.going.domain.pin.service.PinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/pins")
+public class PinController {
+
+    private final PinService pinService;
+
+    @PostMapping
+    public ResponseEntity<Void> createPin(@Valid @RequestBody PinRequestDto requestDto) {
+        pinService.create(requestDto);
+        return ResponseDto.noContent();
+    }
+
+}
