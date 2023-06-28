bd85b98c790280b569c99f6bec1822356dfd48c4
MoonStar <moonsung0331@gmail.com>
2023-06-29 06:38:02 +0900

    feat: 홈 화면 조회 비즈니스 로직 구현

---
+++ src/main/java/com/hackathon/going/domain/travel/controller/TravelController.java
@@ -0,0 +1,31 @@
+package com.hackathon.going.domain.travel.controller;
+
+import com.hackathon.going.domain.common.ResponseDto;
+import com.hackathon.going.domain.post.dto.PostDto;
+import com.hackathon.going.domain.post.service.PostService;
+import com.hackathon.going.domain.travel.dto.TravelDto;
+import com.hackathon.going.domain.travel.response.HomeResponse;
+import com.hackathon.going.domain.travel.service.TravelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/travel")
+@RequiredArgsConstructor
+public class TravelController {
+
+    private final TravelService travelService;
+    private final PostService postService;
+
+    @GetMapping
+    public ResponseDto<HomeResponse> home(Authentication authentication) {
+        TravelDto myTravel = travelService.getMyTravel(authentication.getName());
+        List<PostDto> posts = postService.getPosts();
+        return new ResponseDto<>(new HomeResponse(myTravel, posts));
+    }
+}
