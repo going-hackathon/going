9ec95f76361708811eac8f174ab9c8d411c6e36b
MoonStar <moonsung0331@gmail.com>
2023-06-29 03:29:40 +0900

    feat: pin entity, pin Repository 작성

---
+++ src/main/java/com/hackathon/going/domain/pin/entity/Pin.java
@@ -0,0 +1,31 @@
+package com.hackathon.going.domain.pin.entity;
+
+import com.hackathon.going.domain.common.BaseEntity;
+import com.hackathon.going.domain.travel.entity.Travel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Point;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "pin")
+@Entity
+public class Pin extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pin_id")
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "GEOMETRY")
+    private Point geography;
+
+    @ManyToOne
+    @JoinColumn(name = "travel_id")
+    private Travel travel;
+}
