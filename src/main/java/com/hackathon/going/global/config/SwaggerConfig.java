e8d08800a3179bb176697b942e09bd09852ccafd
siyeonSon <siyeon0209@naver.com>
2023-06-29 02:55:44 +0900

    feat: swagger 도입

---
+++ src/main/java/com/hackathon/going/global/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.hackathon.going.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Going")
+                        .description("Going Server API 명세서입니다.")
+                        .version("v0.0.1"));
+    }
+
+}
