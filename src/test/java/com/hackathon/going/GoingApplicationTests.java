9c8489b497d33587f3da5e1e8b552e062e9f83b2
MoonStar <moonsung0331@gmail.com>
2023-06-29 01:29:33 +0900

    Project Setup

---
+++ src/test/java/com/hackathon/going/GoingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.going;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
