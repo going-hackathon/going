3af732c0bfa16f263dc7bded58496159b070763a
siyeonSon <siyeon0209@naver.com>
2023-06-29 06:17:11 +0900

    refactor: ResponseDto 패키지 위치 원상복귀

--- src/main/java/com/hackathon/going/domain/common/BaseEntity.java
+++ src/main/java/com/hackathon/going/domain/common/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.hackathon.going.domain.common.entity;
+package com.hackathon.going.domain.common;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
