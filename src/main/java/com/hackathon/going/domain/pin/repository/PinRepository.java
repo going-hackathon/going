1cf5245c67d4c08c6661b74112e432c705aed8e6
siyeonSon <siyeon0209@naver.com>
2023-06-30 05:39:41 +0900

    remove: 사용하지 않는 코드 제거

--- src/main/java/com/hackathon/going/domain/pin/repository/PinRepository.java
+++ src/main/java/com/hackathon/going/domain/pin/repository/PinRepository.java
@@ -6,10 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface PinRepository extends JpaRepository<Pin, Long> {
-    Optional<Pin> findByTravelOrderByModifiedAt(Travel travel);
     List<Pin> findAllByTravelOrderByStartDate(Travel travel);
 }
