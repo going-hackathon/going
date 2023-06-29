7b3cee33bf6e2d1b7080194d521596888d379bf6
siyeonSon <siyeon0209@naver.com>
2023-06-29 23:24:56 +0900

    chore: 지역 검색 컨트롤러 로직 생성

--- src/main/java/com/hackathon/going/search/controller/SearchController.java
+++ src/main/java/com/hackathon/going/search/controller/SearchController.java
@@ -1,8 +1,11 @@
 package com.hackathon.going.search.controller;
 
+import com.hackathon.going.domain.common.ResponseDto;
+import com.hackathon.going.search.response.NaverLocationInfoListResponse;
 import com.hackathon.going.search.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,9 +21,9 @@ public class SearchController {
     private final SearchService searchService;
 
     @GetMapping
-    public void searchMusic(
+    public HttpEntity<NaverLocationInfoListResponse> searchMusic(
             @RequestParam("keyword") @NotBlank String keyword
     ) throws IOException, ParseException {
-        searchService.searchLocation(keyword);
+        return ResponseDto.ok(searchService.searchLocation(keyword));
     }
 }
