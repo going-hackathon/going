b8975b14c647e9eb02d2fa5383e29bf0823cab41
siyeonSon <siyeon0209@naver.com>
2023-06-30 05:38:40 +0900

    chore: pin 다중 조회 반환값 추가

--- src/main/java/com/hackathon/going/domain/pin/dto/PinDto.java
+++ src/main/java/com/hackathon/going/domain/pin/dto/PinDto.java
@@ -1,5 +1,6 @@
 package com.hackathon.going.domain.pin.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.hackathon.going.domain.pin.constant.PinStatus;
 import com.hackathon.going.domain.pin.entity.Pin;
 import com.hackathon.going.domain.pinImage.dto.PinImageDto;
@@ -8,7 +9,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Column;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +25,21 @@ public class PinDto {
     private Long id;
     private Double latitude;
     private Double longitude;
+    private String content;
+    private String title;
+    private String address;
+    @JsonFormat(
+            shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy-MM-dd HH:mm:ss",
+            locale = "Asia/Seoul"
+    )
+    private LocalDateTime startDate;
+    @JsonFormat(
+            shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy-MM-dd HH:mm:ss",
+            locale = "Asia/Seoul"
+    )
+    private LocalDateTime endDate;
     private TravelDto travel;
     private PinStatus status;
     private List<PinImageDto> pinImages;
@@ -30,6 +49,11 @@ public class PinDto {
                 .id(entity.getId())
                 .latitude(entity.getLatitude())
                 .longitude(entity.getLongitude())
+                .content(entity.getContent())
+                .title(entity.getTitle())
+                .address(entity.getAddress())
+                .startDate(entity.getStartDate())
+                .endDate(entity.getEndDate())
                 .travel(TravelDto.fromEntity(entity.getTravel()))
                 .status(entity.getStatus())
                 .pinImages(entity.getPinImages().stream()
